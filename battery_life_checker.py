18f63295b801d5b9fc222a242c26dae26550b01a
Michal Pajtinka <michalpajtinka@gmail.com>
2020-10-20 17:15:00 +0200

    comments added

--- battery_life_checker.py
+++ battery_life_checker.py
@@ -1,16 +1,17 @@
-#batterylife_checker
-#@author: Monika
-
-
-
+# @file: batterylife_checker.py
+# @author: Monika
 
 import psutil
 from plyer import notification
 import time
 
+# create battery sensor object
 battery=psutil.sensors_battery()
 while True:
+    # get current battery state
     percent=battery.percent
+    
+    # if battery is too low, throw a notification
     if percent <= 20:
         notification.notify(
             title="Battery Percentage",
@@ -19,4 +20,5 @@ while True:
         )
         break
 
+    # check once in 5 minutes
     time.sleep(60*5)
